7873f9ca57f5394d2a1930214b767f9d407d623d
Andrei <zbu_04@yahoo.com>
2025-11-13 23:58:30 +0100

    changes made

--- SeleniumHomework/src/test/java/SeleniumHomework.java
+++ SeleniumHomework/src/test/java/SeleniumHomework.java
@@ -9,21 +9,34 @@ import java.time.Duration;
 public class SeleniumHomework {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
         //TODO 01.
+
         driver.get("https://9gag.com/");
+
+        //TODO COOKIES
+
+         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector("iframe[id^='sp_message_iframe_']")));
+
+         WebElement acceptBtn = wait.until(
+                 ExpectedConditions.elementToBeClickable(By.cssSelector("button[title='Accept']"))
+         );
+        acceptBtn.click();
+
+        driver.switchTo().defaultContent();
+
 
         //TODO 02. //nu functioneza - o such element: Unable to locate element
        // WebElement oldMeme = driver.findElement(By.xpath("//div[contains(text(), \"Old Meme\")]"));
-        //oldMeme.click();
+      //  oldMeme.click();
 
         //TODO 03.
         WebElement newsButton = driver.findElement(By.xpath("//div[@class=\"home-tab-bar__tab home-tab-bar__tab-news\"]"));
         newsButton.click();
 
         //TODO 04 //nu functioneza - no such element: Unable to locate element: {"method":"xpath","selector":"//a[@href="/trending"]"}
-        // WebElement trendingButton = driver.findElement(By.xpath("//a[@href=\"/trending\"]"));
+        //WebElement trendingButton = driver.findElement(By.xpath("//a[@href=\"/trending\"]"));
         //trendingButton.click();
 
 
@@ -32,50 +45,12 @@ public class SeleniumHomework {
         potatozButton.click();
 
         //TODO 06.
-        //WebElement searchInput = driver.findElement(By.xpath("//input[@placeholder='Search...']"));
-        //searchInput.sendKeys("'happy to know that are still good people in the world'");
+        WebElement searchButton = driver.findElement(By.xpath("//a[@class=\"search\"]"));
+        searchButton.click();
 
-        WebElement searchInput = wait.until (
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Search...']"))
-        );
+        WebElement searchInput = driver.findElement(By.xpath("//input[@placeholder='Search...']"));
         searchInput.sendKeys("'happy to know that are still good people in the world'");
-
-       // CookieUtils.acceptCookiesIfPresent(driver);
-
-
-
-      /*  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement cookie = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(
-                        By.cssSelector("div > button[title=\"Accept\"]"))
-                );
-
-        cookie.click();*/
-       /* JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement cookie = driver.findElement(By.xpath("//div[@id='sp_message_container_1395349']"));
-        js.executeScript("arguments[0].setAttribute('style', 'display:none')", cookie);*/
-
-
-       // cookie.click();
-
-        //remove cookies
-     /*   WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get("https://www.9gag.com");
-        try {
-            WebElement acceptButton = wait.until(
-                    ExpectedConditions.elementToBeClickable(
-                            By.xpath("//button[@title='Accept']")
-                    )
-            );
-            acceptButton.click();
-            System.out.println("Accept button clicked");
-        } catch (TimeoutException e) {
-            System.out.println("Accept button timed out");
-            System.out.println(e.getMessage());
-        }
-        driver.manage().deleteAllCookies();*/
-
-
+        searchInput.submit();
 
     }
 }
