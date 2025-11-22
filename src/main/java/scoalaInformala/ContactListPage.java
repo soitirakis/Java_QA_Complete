3ac495ad162883b54d14ef57f5c06a217f41cfad
Razvan <ionut.pruteanu@outlook.com>
2025-11-22 17:59:05 +0200

    ordered a bit the page structure due to some accidental commits into master. Modified ContactListPage to use PageFactory which allows for elements to be loaded when needed (lazy loading) and not when the constructor is being called this allows to declare fields with @FindBy instead of repeatedly calling driver.findElement. Also, Lazy lookup reduces wasted finds and can help with pages that load elements later.
    Modified Login test so that it uses the updated naming

---
+++ src/main/java/scoalaInformala/ContactListPage.java
@@ -0,0 +1,29 @@
+package scoalaInformala;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ContactListPage {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public ContactListPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+
+    @FindBy(xpath = "//h1[text()='Contact List']")
+    public WebElement contactListHeader;
+
+    @FindBy(xpath = "//button[@id='add-contact']")
+    public WebElement addANewContact;
+
+    
+}
