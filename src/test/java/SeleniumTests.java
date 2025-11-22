f5986ed3abe28841a7007eebf4958a17610790df
Andrei <zbu_04@yahoo.com>
2025-11-22 12:23:37 +0100

    login method added using user. assertion logout, logout checked.

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -16,6 +16,11 @@ public class SeleniumTests {
 
     @Test
     public static void createUser(){
+        String userFirstName = "Andrei";
+        String userLastName = "Andy";
+        String userEmail = "andrei123@example.com";
+        String userPassword = "andrei123";
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://thinking-tester-contact-list.herokuapp.com/");
@@ -25,16 +30,20 @@ public class SeleniumTests {
         signUpButton.click();
 
         WebElement firstNameInput = driver.findElement(By.id("firstName"));
-        firstNameInput.sendKeys("Razvan");
+        //firstNameInput.sendKeys("Razvan");
+        firstNameInput.sendKeys(userFirstName);
 
         WebElement lastNameInput = driver.findElement(By.id("lastName"));
-        lastNameInput.sendKeys("Pruteanu");
+        //lastNameInput.sendKeys("Pruteanu");
+        lastNameInput.sendKeys(userLastName);
 
         WebElement emailInput = driver.findElement(By.id("email"));
-        emailInput.sendKeys(Utils.generateRandomEmail());
+        //emailInput.sendKeys(Utils.generateRandomEmail());
+        emailInput.sendKeys(userEmail);
 
         WebElement passwordInput = driver.findElement(By.id("password"));
-        passwordInput.sendKeys("JKDHAKDHiiufd*&*&98_");
+        //passwordInput.sendKeys("JKDHAKDHiiufd*&*&98_");
+        passwordInput.sendKeys(userPassword);
 
         WebElement submitButton = driver.findElement(By.id("submit"));
         submitButton.click();
@@ -43,11 +52,34 @@ public class SeleniumTests {
         Assert.assertTrue(contactListHeaderFound.isDisplayed());
 
         driver.close();
-
-
-
-
     }
 
+    @Test
+    public static void login(){
+        String userEmail = "andrei123@example.com";
+        String userPassword = "andrei123";
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://thinking-tester-contact-list.herokuapp.com/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+
+        WebElement emailInput = driver.findElement(By.id("email"));
+        emailInput.sendKeys(userEmail);
+
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys(userPassword);
+
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+        WebElement logoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout")));
+        Assert.assertTrue(logoutButton.isDisplayed());
+
+        logoutButton.click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://thinking-tester-contact-list.herokuapp.com/"));
+        driver.close();
+    }
 
 }
