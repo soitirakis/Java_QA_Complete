6edb0e5d548e429a8ecb68000a3f5973ae59a20f
burlacugeorge <burlacugeorge22@gmail.com>
2025-11-22 13:31:39 +0200

    Am modificat datele de logare si am creat Webelement-ul "logoutButton"

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -15,40 +15,52 @@ public class SeleniumTests {
 
 
     @Test
-    public static void createUser() {
+//    public static void createUser() {
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://thinking-tester-contact-list.herokuapp.com/");
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+//
+//        WebElement signUpButton = driver.findElement(By.id("signup"));
+//        signUpButton.click();
+//
+//        WebElement firstNameInput = driver.findElement(By.id("firstName"));
+//        firstNameInput.sendKeys("Razvan");
+//
+//        WebElement lastNameInput = driver.findElement(By.id("lastName"));
+//        lastNameInput.sendKeys("Pruteanu");
+//
+//        WebElement emailInput = driver.findElement(By.id("email"));
+//        emailInput.sendKeys(Utils.generateRandomEmail());
+//
+//        WebElement passwordInput = driver.findElement(By.id("password"));
+//        passwordInput.sendKeys("JKDHAKDHiiufd*&*&98_");
+//
+//        WebElement submitButton = driver.findElement(By.id("submit"));
+//        submitButton.click();
+//
+//        WebElement contactListHeaderFound = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text() = 'Contact List']")));
+//        Assert.assertTrue(contactListHeaderFound.isDisplayed());
+//
+//        driver.close();
+//    }
+
+    public static void logoutUser() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://thinking-tester-contact-list.herokuapp.com/");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        WebElement signUpButton = driver.findElement(By.id("signup"));
-        signUpButton.click();
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("georgeburlacu@gmail.com");
 
-        WebElement firstNameInput = driver.findElement(By.id("firstName"));
-        firstNameInput.sendKeys("George");
-
-        WebElement lastNameInput = driver.findElement(By.id("lastName"));
-        lastNameInput.sendKeys("Burlacu");
-
-        WebElement emailInput = driver.findElement(By.id("email"));
-        emailInput.sendKeys(Utils.generateRandomEmail());
-
-        WebElement passwordInput = driver.findElement(By.id("password"));
-        passwordInput.sendKeys("df4rgt6!");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("georgeburlacu91@");
 
         WebElement submitButton = driver.findElement(By.id("submit"));
         submitButton.click();
 
-        WebElement contactListHeaderFound = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text() = 'Contact List']")));
-        Assert.assertTrue(contactListHeaderFound.isDisplayed());
-
-        WebElement logoutButton = driver.findElement(By.id("logout"));
+        WebElement logoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout")));
         logoutButton.click();
-
-        driver.close();
-
-
     }
-
-
 }
